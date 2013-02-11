b773844081277ef078b4be8978dcba30114173d2
ArlindNocaj <arlind.nocaj@gmail.com>
2013-02-11 13:50:07 +0100

    added small example for usage

--- src/kn/uni/voronoitreemap/diagram/PowerDiagram.java
+++ src/kn/uni/voronoitreemap/diagram/PowerDiagram.java
@@ -310,46 +310,47 @@ public class PowerDiagram {
 	}
 
 	public static void main(String[] args) {
+		
 		PowerDiagram diagram = new PowerDiagram();
 
+		//normal list based on an array
 		OpenList sites = new OpenList();
 
-		Random rand = new Random();
-		int amount = 200;
-
+		Random rand = new Random(100);
+			//  create a root polygon which limits the voronoi diagram.
+		//  here it is just a rectangle.
+		 
 		PolygonSimple rootPolygon = new PolygonSimple();
-		int width = 2000;
-		int height = 2000;
+		int width = 1000;
+		int height = 1000;
 		rootPolygon.add(0, 0);
 		rootPolygon.add(width, 0);
 		rootPolygon.add(width, height);
 		rootPolygon.add(0, height);
-		for (int i = 0; i < amount; i++) {
+		
+		
+		// create 100 points (sites) and set random positions in the rectangle defined above.
+		for (int i = 0; i < 100; i++) {
 			Site site = new Site(rand.nextInt(width), rand.nextInt(width));
+			// we could also set a different weighting to some sites
+			// site.setWeight(30)
 			sites.add(site);
 		}
-		// double factor=10;
-		// Site[] array = sites.array;
-		// int size=sites.size;
-		// for (int z=0;z<size;z++){
-		// Site s = array[z];
-		// double x=s.getX()*factor;
-		// double y=s.getY()*factor;
-		// double weight=s.getWeight()*factor*factor;
-		// s.setXYW(x, y, weight);
-		// }
-
+		
+		// set the list of points (sites), necessary for the power diagram
 		diagram.setSites(sites);
-
+		// set the clipping polygon, which limits the power voronoi diagram
 		diagram.setClipPoly(rootPolygon);
-		long start = System.currentTimeMillis();
+		
+		// do the computation
 		diagram.computeDiagram();
-		// diagram.showDiagram();
-
-		long end = System.currentTimeMillis();
-		double seconds = end - start;
-		seconds = seconds / 1000.0;
-		System.out.println("Needed seconds: " + seconds);
+		
+		// for each site we can no get the resulting polygon of its cell. 
+		// note that the cell can also be empty, in this case there is no polygon for the corresponding site.
+		for (int i=0;i<sites.size;i++){
+			Site site=sites.array[i];
+			PolygonSimple polygon=site.getPolygon();
+		}
 	}
 
 	public void showDiagram() {
